91a56c0bc502809f18b4bd0d5da8a521079e6d77
Rafaella Ladeira <rafaella.ladeira@gmail.com>
2022-05-25 16:27:41 -0300

    Correções

--- desafio10.sql
+++ desafio10.sql
@@ -1,3 +1,1 @@
-SELECT * FROM northwind.purchase_orders
-WHERE cretead_by >= 3
-ORDER BY created_by DESC, id DESC;
+SELECT * FROM northwind.purchase_orders WHERE cretead_by >= 3 ORDER BY created_by DESC, id DESC;
